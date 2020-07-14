afc9c9ed5c21f6111255179c1e02ed086344064c
monomadic <deathdisco@protonmail.com>
2020-07-14 21:09:12 +1000

    switched from markdown-rs to pulldown-cmark

--- src/markdown.rs
+++ src/markdown.rs
@@ -1,8 +1,13 @@
 use crate::error::*;
-use markdown;
+use pulldown_cmark::{html, Event, Options, Parser, Tag};
+use std::io::{stdout};
 
 pub fn parse(i: &str) -> AstryxResult<String> {
     // TODO use a stricter lib that will throw errors, or
     // write one that returns a syntax tree of nodes
-    Ok(markdown::to_html(i))
+    let parser = Parser::new_ext(i, Options::empty());
+
+    let mut buf: Vec<u8> = Vec::new();
+    html::write_html(&mut buf, parser).unwrap();
+    Ok(String::from_utf8(buf).unwrap())
 }
