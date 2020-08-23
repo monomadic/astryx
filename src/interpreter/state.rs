29b31a91cedcde82bcbb1069b5cbf14071fbde9b
monomadic <deathdisco@protonmail.com>
2020-08-23 17:13:22 +1000

    state isolation

--- src/interpreter/state.rs
+++ src/interpreter/state.rs
@@ -4,13 +4,13 @@ use rctree::Node;
 use parser::variable::Variable;
 
 type LocalData = HashMap<String, Variable>;
-type PageBuffers = HashMap<String, Node<HTMLNode>>;
+type Layouts = HashMap<String, Node<HTMLNode>>;
 
 // TODO make private type
 #[derive(Debug, Clone)]
 pub(crate) struct State {
     local_variables: LocalData,
-    pub(crate) pages: PageBuffers,
+    pub(crate) pages: Layouts,
     imports: Imports,
 }
 
@@ -18,7 +18,7 @@ impl State {
     pub fn new() -> Self {
         State {
             local_variables: LocalData::new(),
-            pages: PageBuffers::new(),
+            pages: Layouts::new(),
             imports: Imports::new(),
         }
     }
