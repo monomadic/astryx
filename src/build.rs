e1886355c5b53238509455f5b299b9655e79b610
monomadic <deathdisco@protonmail.com>
2020-10-11 23:49:32 +1100

    temporarily turned off html output

--- src/build.rs
+++ src/build.rs
@@ -5,7 +5,7 @@ pub(crate) fn build<'a>(path: &str) -> AstryxResult<'a, ()> {
     let file = std::fs::read_to_string(path)?;
 
     match crate::render::render(&file) {
-        Ok(buffers) => println!("ok. {:?}", buffers),
+        Ok(buffers) => println!("ok. {:#?}", buffers),
         Err(e) => display_error(e, &path, &file),
     }
     Ok(())
