d7517a0361960882672356b29a14daa85fd26a90
monomadic <deathdisco@protonmail.com>
2020-11-17 17:12:38 +1100

    preparing for new style of function application

--- components/interpreter/src/builtins.rs
+++ components/interpreter/src/builtins.rs
@@ -2,7 +2,7 @@ use crate::{models::Object, InterpreterResult, State};
 use std::cell::RefCell;
 use std::rc::Rc;
 
-pub(crate) fn print(args: Vec<Object>) -> InterpreterResult<Object> {
+pub(crate) fn log(args: Vec<Object>) -> InterpreterResult<Object> {
     let display = args
         .iter()
         .map(|o| format!("{}", o.to_string()))
@@ -26,13 +26,19 @@ pub(crate) fn debug(args: Vec<Object>) -> InterpreterResult<Object> {
 
 pub(crate) fn import<'a>(state: Rc<RefCell<State<'a>>>) -> Rc<RefCell<State<'a>>> {
     // let mut inner = state.borrow_mut();
-    let _ = state
-        .borrow_mut()
-        .bind("print", Object::BuiltinFunction(print));
+    let _ = state.borrow_mut().bind("log", Object::BuiltinFunction(log));
 
     let _ = state
         .borrow_mut()
         .bind("debug", Object::BuiltinFunction(debug));
 
+    let _ = state
+        .borrow_mut()
+        .bind("markdown", Object::BuiltinFunction(markdown));
+
     state
+}
+
+pub(crate) fn markdown<'a>(args: Vec<Object>) -> InterpreterResult<Object> {
+    Ok(Object::String("markdownnnnn".into()))
 }
