be2e038a40ee9cdd873cfb4f0f5408ca4b99b5c9
monomadic <deathdisco@protonmail.com>
2020-12-18 06:48:51 +1100

    improved line error test case

--- components/parser/src/lib.rs
+++ components/parser/src/lib.rs
@@ -66,6 +66,10 @@ fn parse_line<'a>(line: Line<'a>) -> IResult<Span<'a>, Node<Statement<'a>>, Pars
     Ok((r, node))
 }
 
+pub fn parse<'a>(i: Span<'a>) -> IResult<Span<'a>, Statement<'a>, ParserError<Span<'a>>> {
+    statement::statement(i)
+}
+
 // #[test]
 // fn test_run() {
 //     assert!(run("".into()).is_ok());
--- components/parser/tests/integration.rs
+++ components/parser/tests/integration.rs
@@ -23,19 +23,20 @@ fn test_parser() {
 
 #[test]
 fn test_error_position() {
-    fn assert_position(i: &str, line: u32, offset: usize) {
-        let error = run(i).unwrap_err();
+    fn assert_error_position(i: &str, line: u32, column: usize) {
+        let input = Span::new(i);
+        let err = parser::parse(input).unwrap_err();
 
-        assert_eq!(error.context.offset, offset);
-
-        assert_eq!(
-            error.context,
-            LocatedSpan {
-                offset,
-                line,
-                fragment: "",
-                extra: (),
-            }
-        );
+        if let nom::Err::Error(e) = err {
+            let (s, pos) = position::<Span<'_>, ParserError<Span<'_>>>(e.pos).unwrap();
+            assert_eq!(pos.get_column(), column);
+            assert_eq!(pos.location_line(), line);
+        // assert_eq!(pos.location_offset(), line);
+        } else {
+            panic!("error not returned!");
+        }
     }
+
+    assert_error_position("!", 1, 1);
+    assert_error_position("print()!", 1, 8);
 }
