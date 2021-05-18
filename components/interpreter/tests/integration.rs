0e4d8fc960e3e3c5dc483975f2ee593c1f438c09
nom <nom@nom.org>
2021-05-18 15:22:49 +0800

    fixed integration test

--- components/interpreter/tests/integration.rs
+++ components/interpreter/tests/integration.rs
@@ -7,11 +7,11 @@ use std::rc::Rc;
 fn eval(input: &str) {
     let state = Rc::new(RefCell::new(State::new()));
 
-    let site = nom_indent::indent(input, "<test>")
+    let site: Site = nom_indent::indent(input, "<test>")
         .map_err(|_e| AstryxError::Generic("blah".into()))
         .and_then(|(_, lines)| parser::parse(lines))
         .and_then(|statements| interpreter::run(&statements, state))
-        .map(Site::render)
+        .map(|objects| objects.into())
         .unwrap();
 
     println!("{:?}", site);
